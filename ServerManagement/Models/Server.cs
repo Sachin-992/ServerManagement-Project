c7b3cbeb89503dbec0a9838e73f2e55b97d13a48
Sachin-992 <sachinchinnasamy2021@gmail.com>
2025-02-05 14:52:43 +0530

    Add project files.

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0? false : true;  //ternary operator 
+        }
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+
+        [Required]
+        public string? Name { get; set; } //The ? after string allows it to hold null values.
+        [Required]
+        public string? City { get; set; }
+    }
+}
